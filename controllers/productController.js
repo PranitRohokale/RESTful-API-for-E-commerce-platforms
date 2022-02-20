c88c6e907ef18687542e4b13c8190687ed77ab98
PranitRohokale <pranitrohokale@gmail.com>
2022-02-20 14:11:09 +0530

    update , delete products done!

--- controllers/productController.js
+++ controllers/productController.js
@@ -43,18 +43,99 @@ exports.createProduct = BigPromise(async (req, res, next) => {
 });
 
 exports.getAllProducts = BigPromise(async (req, res, next) => {
-  const resultPerPage = 6
+  const resultPerPage = 6;
   const totalProducts = await Product.countDocuments();
 
-  const productObj = new WhereClause(Product.find(), req.query).search().filter()
-  productObj.pager(resultPerPage)
+  const productObj = new WhereClause(Product.find(), req.query)
+    .search()
+    .filter();
+  productObj.pager(resultPerPage);
 
-  const products = await productObj.base.clone()
+  const products = await productObj.base.clone();
 
   res.status(200).json({
     success: true,
     products,
     totalProducts,
-    searchResultCount : products.length
+    searchResultCount: products.length,
+  });
+});
+
+exports.getOneProduct = BigPromise(async (req, res, next) => {
+  const product = await Product.findById(req.params.id);
+
+  if (!product) {
+    return next(new CustomeError("Product does not exits!", 403));
+  }
+
+  res.status(200).json({
+    success: true,
+    product,
+  });
+});
+
+exports.updateProduct = BigPromise(async (req, res, next) => {
+  let product = await Product.findById(req.params.id);
+
+  if (!product) return next(new CustomeError("Product not exists!", 400));
+  
+
+
+  if (req.files && req.files.photo) {
+    //deleting earlier photos
+    for (let i = 0; i < product.photos.length; i++) {
+      const res = await cloudinary.v2.uploader.destroy(product.photos[i].id);
+      
+    }
+    console.log(`deleted`);
+    //uploading new products
+    let photoArray = [];
+    for (let i = 0; i < req.files.photo.length; i++) {
+      const result = await cloudinary.v2.uploader.upload(
+        req.files.photo[i].tempFilePath,
+        {
+          folder: "Products",
+        }
+      );
+
+      photoArray.push({
+        id: result.public_id,
+        secure_url: result.secure_url,
+      });
+    }
+    
+    req.body.photos = photoArray;
+  }
+
+  req.body.user = req.user._id;
+  console.log(req.body);
+  product = await Product.findByIdAndUpdate(req.params.id, req.body, {
+    new: true,
+    runValidators: true,
+  });
+
+  res.status(200).json({
+    success: true,
+    product,
+  });
+
+  console.log(product);
+});
+
+exports.deleteProduct = BigPromise(async (req, res, next) => {
+  const product = await Product.findById(req.params.id);
+
+  if (!product) return next(new CustomeError("Product not exists!", 400));
+
+  //deleting earlier photos
+  for (let i = 0; i < product.photos.length; i++) {
+    await cloudinary.v2.uploader.destroy(product.photos[i].id);
+  }
+
+  await product.remove();
+
+  res.status(200).json({
+    success: true,
+    message: "Product was removed!",
   });
 });
--- routers/product.js
+++ routers/product.js
@@ -2,17 +2,32 @@ const express = require("express");
 const router = express.Router();
 
 //controllers
-const { testProduct, createProduct, getAllProducts } = require("../controllers/productController");
+const {
+  testProduct,
+  createProduct,
+  getAllProducts,
+  getOneProduct,
+  updateProduct,
+  deleteProduct,
+} = require("../controllers/productController");
 //middlewares
-const {isLoggedIn,customRole} = require("../middlewares/user")
+const { isLoggedIn, customRole } = require("../middlewares/user");
 
 //GET
 router.route("/testProduct").get(testProduct);
 
-// USER 
-router.route("/getProducts").get(isLoggedIn,getAllProducts)
+// USER
+router.route("/getProducts").get(isLoggedIn, getAllProducts);
+router.route("/product/:id").get(isLoggedIn, getOneProduct);
 
-// ADMIN 
-router.route("/products/create").post(isLoggedIn,customRole('admin'),createProduct)
+// ADMIN
+router
+  .route("/product/create")
+  .post(isLoggedIn, customRole("admin"), createProduct);
+
+router
+  .route("/product/:id")
+  .put(isLoggedIn, customRole("admin"), updateProduct)
+  .delete(isLoggedIn, customRole("admin"), deleteProduct);
 
 module.exports = router;
